5a8693855d1ff759288033a5178a1dbc2afa256c
zhangbenxing <zhangbenxing@kuandeng.com>
2018-01-31 14:45:38 +0800

    增加PYTHONPATH

--- utils.py
+++ utils.py
@@ -7,11 +7,7 @@ import paramiko
 from collections import namedtuple
 
 Label = namedtuple(
-    'Label', ['name', 'id', 'classId', 'className', 'categoryId',
-              'hasInstances', 'ignoreInEval', 'color'])
-
-Label1 = namedtuple(
-    'Label', ['id', 'categoryId', 'color', 'name'])
+    'Label', ['en_name', 'id', 'categoryId', 'color', 'name'])
 
 port = 7022
 host_ip = "192.168.5.38"
@@ -19,49 +15,26 @@ context = "kss-auto-training"
 max_packages = 100000
 
 self_road_chn_labels = {
-    Label(u'车道标线', 0, 0, 'Lane', 0, True, False, (255, 0, 0)),
-    Label(u'左侧道路边缘线', 1, 1, 'Symbol', 1, True, False, (255, 192, 203)),
-    Label(u'右侧道路边缘线', 2, 2, 'SpecialLane', 2, True, False, (139, 0, 139)),
-    Label(u'纵向减速标线', 3, 1, 'Symbol', 3, True, False, (32, 128, 192)),
-    Label(u'专用车道标线', 4, 2, 'SpecialLane', 4, True, False, (192, 128, 255)),
-    Label(u'停止线', 5, 3, 'StopLane', 5, True, False, (255, 128, 64)),
-    Label(u'减速让行标线', 6, 4, 'SlowLane', 6, True, False, (0, 255, 255)),
-    Label(u'减速标线/减速带', 7, 5, 'SlowNet', 7, True, False, (128, 128, 255)),
-    Label(u'人行横道', 8, 14, 'SlowNet', 8, True, False, (128, 192, 192)),
-    Label(u'路面连接带', 9, 15, 'SlowNet', 9, True, False, (128, 128, 192)),
-    Label(u'停靠站标线', 10, 15, 'SlowNet', 10, True, False, (240, 128, 128)),
-    Label(u'出入口标线', 11, 6, 'InOut', 11, True, False, (128, 128, 0)),
-    Label(u'文字符号类', 12, 7, 'FishLane', 12, True, False, (0, 0, 255)),
-    Label(u'导流线（鱼刺线）', 13, 8, 'FishLane', 13, True, False, (0, 255, 0)),
-    Label(u'停止网格标线', 14, 9, 'StopNet', 14, True, False, (255, 255, 0)),
-    Label(u'车距确认线', 15, 10, 'Sidewalk', 15, True, False, (255, 128, 255)),
-    Label(u'道路', 16, 11, 'Road', 16, True, False, (192, 192, 192)),
-    Label(u'其他', 17, 12, 'Other', 17, True, False, (64, 64, 32)),
-    Label(u'车辆及路面上其他物体', 18, 12, 'Other', 18, True, False, (128, 0, 0)),
-    Label(u'Ignore', 19, 13, 'Other', 19, True, False, (0, 0, 0)),
-}
-
-self_road_simple_labels = {
-    Label1(0, 0,     (255, 0, 0),        u'车道标线'),
-    Label1(1, 1,     (255, 192, 203),    u'左侧道路边缘线'),
-    Label1(2, 2,     (139, 0, 139),      u'右侧道路边缘线'),
-    Label1(3, 3,     (32, 128, 192),     u'纵向减速标线'),
-    Label1(4, 4,     (192, 128, 255),    u'专用车道标线'),
-    Label1(5, 5,     (255, 128, 64),     u'停止线'),
-    Label1(6, 6,     (0, 255, 255),      u'减速让行标线'),
-    Label1(7, 7,     (128, 128, 255),    u'减速标线/减速带'),
-    Label1(8, 8,     (128, 192, 192),    u'人行横道'),
-    Label1(9, 9,     (128, 128, 192),    u'路面连接带'),
-    Label1(10, 10,   (240, 128, 128),    u'停靠站标线'),
-    Label1(11, 11,   (128, 128, 0),      u'出入口标线'),
-    Label1(12, 12,   (0, 0, 255),        u'文字符号类'),
-    Label1(13, 13,   (0, 255, 0),        u'导流线（鱼刺线）'),
-    Label1(14, 14,   (255, 255, 0),      u'停止网格标线'),
-    Label1(15, 15,   (255, 128, 255),    u'车距确认线'),
-    Label1(16, 16,   (192, 192, 192),    u'道路'),
-    Label1(17, 17,   (64, 64, 32),       u'其他'),
-    Label1(18, 18,   (128, 0, 0),        u'车辆及路面上其他物体'),
-    Label1(19, 19,   (0, 0, 0),          u'Ignore'),
+    Label('other',          0, 0,     (64, 64, 32),       u'其他'),
+    Label('ignore',         1, 1,     (0, 0, 0),          u'Ignore'),
+    Label('lane',           2, 2,     (255, 0, 0),        u'车道标线'),
+    Label('left',           3, 3,     (255, 192, 203),    u'左侧道路边缘线'),
+    Label('right',          4, 4,     (139, 0, 139),      u'右侧道路边缘线'),
+    Label('v_slow',         5, 5,     (32, 128, 192),     u'纵向减速标线'),
+    Label('bus_lane',       6, 6,     (192, 128, 255),    u'专用车道标线'),
+    Label('stop',           7, 7,     (255, 128, 64),     u'停止线'),
+    Label('slow_let',       8, 8,     (0, 255, 255),      u'减速让行标线'),
+    Label('slow_zone',      9, 9,     (128, 128, 255),    u'减速标线/减速带'),
+    Label('sidewalk',       10, 10,   (128, 192, 192),    u'人行横道'),
+    Label('connection',     11, 11,   (128, 128, 192),    u'路面连接带'),
+    Label('stop_station',   12, 12,   (240, 128, 128),    u'停靠站标线'),
+    Label('in_out',         13, 13,   (128, 128, 0),      u'出入口标线'),
+    Label('symbol',         14, 14,   (0, 0, 255),        u'文字符号类'),
+    Label('fish_lane',      15, 15,   (0, 255, 0),        u'导流线（鱼刺线）'),
+    Label('stop_gird',      16, 16,   (255, 255, 0),      u'停止网格标线'),
+    Label('distance',       17, 17,   (255, 128, 255),    u'车距确认线'),
+    Label('road',           18, 18,   (192, 192, 192),    u'道路'),
+    Label('objects',        19, 19,   (128, 0, 0),        u'车辆及路面上其他物体'),
 }
 
 
