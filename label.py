9c788d51e3a2947e2ebe20474a99ce0c27610da8
zhangbenxing <zhangbenxing@kuandeng.com>
2018-03-02 15:59:37 +0800

    增加标注校验

---
+++ label.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+Label = namedtuple(
+    'Label', ['en_name', 'id', 'categoryId', 'color', 'name'])
+
+self_road_chn_labels = {
+    Label('other',          0, 0,     (64, 64, 32),       u'其他'),
+    Label('ignore',         1, 1,     (0, 0, 0),          u'Ignore'),
+    Label('lane',           2, 2,     (255, 0, 0),        u'车道标线'),
+    Label('left',           3, 3,     (255, 192, 203),    u'左侧道路边缘线'),
+    Label('right',          4, 4,     (139, 0, 139),      u'右侧道路边缘线'),
+    Label('v_slow',         5, 5,     (32, 128, 192),     u'纵向减速标线'),
+    Label('bus_lane',       6, 6,     (192, 128, 255),    u'专用车道标线'),
+    Label('stop',           7, 7,     (255, 128, 64),     u'停止线'),
+    Label('slow_let',       8, 8,     (0, 255, 255),      u'减速让行标线'),
+    Label('slow_zone',      9, 9,     (128, 128, 255),    u'减速标线/减速带'),
+    Label('sidewalk',       10, 10,   (128, 192, 192),    u'人行横道'),
+    Label('connection',     11, 11,   (128, 128, 192),    u'路面连接带'),
+    Label('stop_station',   12, 12,   (240, 128, 128),    u'停靠站标线'),
+    Label('in_out',         13, 13,   (128, 128, 0),      u'出入口标线'),
+    Label('symbol',         14, 14,   (0, 0, 255),        u'文字符号类'),
+    Label('fish_lane',      15, 15,   (0, 255, 0),        u'导流线（鱼刺线）'),
+    Label('stop_gird',      16, 16,   (255, 255, 0),      u'停止网格标线'),
+    Label('distance',       17, 17,   (255, 128, 255),    u'车距确认线'),
+    Label('road',           18, 18,   (192, 192, 192),    u'道路'),
+    Label('objects',        19, 19,   (128, 0, 0),        u'车辆及路面上其他物体'),
+    Label('emergency',      20, 20,   (229, 152, 102),    u'虚拟应急车道线'),
+}
